fcba453388cbdeb7dfc8f9c07a790cdd15dd6a81
Abner2111 <abneraq73@gmail.com>
2023-10-17 22:46:59 -0600

    added analog read and message sending logic

--- Tarea1/Master/Master.ino
+++ Tarea1/Master/Master.ino
@@ -1,5 +1,5 @@
 #include <SPI.h>
-
+const int inputPin = A0;
 const int slaveSelectPin = 10;
 const byte handshakeMessage = 0xAA;
 long timeStamp;
@@ -14,6 +14,8 @@ void setup() {
   // Initialize SPI:
   SPI.begin();
   handshakeDone = false;
+  timeStamp = millis();
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -30,9 +32,28 @@ void loop() {
       // Handshake successful
       Serial.println("Handshake successful!");
       handshakeDone = true;
+      timeStamp = millis();
     } 
   } else {
-    
+    byte response = SPI.transfer(0x00);
+    if (response!=sentMessage){
+      digitalWrite(slaveSelectPin, LOW); //enable slave select
+      SPI.transfer(message); //send message
+      digitalWrite(slaveSelectPin, HIGH); //disable slave select
+    }
+    else{
+      byte message = AnalogRead(inputPin)/128;
+      digitalWrite(slaveSelectPin, LOW); //enable slave select
+      SPI.transfer(message); //send message
+      digitalWrite(slaveSelectPin, HIGH); //disable slave select
+      sentMessage = message;
+      timeStamp = millis();
+    }
+
+    if  ((millis-timeStamp)>=3000){
+      handshakeDone = false; //response took longer than 3 seconds
+      Serial.println("No response");
+    }
   }
   
 
